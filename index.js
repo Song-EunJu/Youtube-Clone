6cb59c5cf6e2e3615a955c3e9caa8d1bc7470fa5
Song-EunJu <jd06280@naver.com>
2021-10-05 22:53:02 +0900

    [ADD] User 객체 추가

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express') // express 모듈을 가져옴
 const app = express() // 새로운 express 앱을 생성 
 const port = 5001
+const { User } = require("./models/User") // models 에서 생성한 User model 을 가져온다
+
+// application/x-www-form-urlencoded 
+
+//app.use(bodyParser.urlencoded({extended: true})); -> express 업데이트 후 body-parser 를 안가져와도 됨
+app.use(express.urlencoded({extended:true}));
+
+// application/json
+app.use(express.json());
+// app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://jd06280:thddmswn99@cluster0.biuf3.mongodb.net/youtube-clone?retryWrites=true&w=majority',{
@@ -12,6 +23,31 @@ mongoose.connect('mongodb+srv://jd06280:thddmswn99@cluster0.biuf3.mongodb.net/yo
 app.get('/', (req, res) => { 
   res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {
+  // 회원가입할 때 필요한 정보를 client 에서 가져오면 데이터베이스에 넣어줌
+  const user = new User(req.body);
+  /*
+    req.body 안에 이런식으로 들어있는 거임
+    {
+      id:"hello",
+      password:"123"
+    } 
+  */
+  user.save((err, doc) => { // mongodb에서 오는 method
+    if(err)
+      return res.json({
+        success: false, err
+      })
+    else
+      return res.status(200).json({
+        success:true
+      }) // status(200) : 성공했다는 뜻
+  });
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
