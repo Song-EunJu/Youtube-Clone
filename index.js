bbcae2f2e5e79239d18643aafade1b489c10f7c7
Song-EunJu <jd06280@naver.com>
2021-10-06 12:07:54 +0900

    [ADD] 로그인 기능

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express'); // express 모듈을 가져옴
 const app = express() // 새로운 express 앱을 생성 
 const port = 5001
 const config = require('./config/key');
+const cookieParser = require('cookie-parser')
 const { User } = require("./models/User"); // models 에서 생성한 User model 을 가져온다
 
 // application/x-www-form-urlencoded 
@@ -12,7 +13,7 @@ app.use(express.urlencoded({extended:true}));
 // application/json
 app.use(express.json());
 // app.use(bodyParser.json());
-
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -39,6 +40,42 @@ app.post('/register', (req, res) => {
         success:true
       }) // status(200) : 성공했다는 뜻
   });
+})
+
+app.post('/login', (req, res) => {
+  // 요청된 이메일이 DB에 있는지 찾는다
+  User.findOne({ email : req.body.email }, (err, user) => {
+    if(!user){ 
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    // 요청된 이메일이 DB에 있다면 비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다."
+        })
+
+      // 비밀번호가 같으면 Token 생성
+      user.generateToken((err, user) => {
+        if(err)
+          return res.status(400).send(err);
+
+        /* 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지 저장 가능
+           이 강의에서는 쿠키 사용 -> 쿠키파서 깔아야 함
+        */
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({
+          loginSuccess: true,
+          userId: user._id
+        })
+      })
+    })
+  })
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -35,7 +36,8 @@ const userSchema = mongoose.Schema({
 userSchema.pre('save', function(next){
     var user = this; // userSchema 를 가리키고 있는 것
 
-    if(user.isModified('password')){ // password가 변경될 때만 처리
+    // 이름이든 이메일이든 어떤 값을 변경할 때마다 매번 비밀번호를 암호화 처리하게 됨
+    if(user.isModified('password')){ // password가 변경될 때만 암호화해야 함
         // 비밀번호 암호화
         bcrypt.genSalt(saltRounds, function(err, salt){
             if(err)
@@ -47,9 +49,40 @@ userSchema.pre('save', function(next){
                 next()
             })
         })      
+    } 
+    else{ // 비밀번호가 아니라 다른 항목을 바꾼 경우에는 그냥 나가기
+        next();
     }
-    
 }) // index.js 에서 user 모델을 저장하기 전에 어떤 동작을 한다.
+
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    /* 
+        plain : 1234567 / 암호화된 비밀번호 : 어쩌구저쩌구 
+        plain을 암호화한 다음에 암호화된 비밀번호랑 같은지 체크해야 함
+    */
+    bcrypt.compare
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err)
+            return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+
+    // json web token 이용해서 토큰 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    // user._id + 'secretToken' = token 생성
+
+    user.token = token
+    user.save(function(err,user){
+        if(err)
+            return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema) 
 module.exports = { User } // 다른 곳에서도 사용할 수 있도록 module화를 해준다
